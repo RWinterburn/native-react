3b164918bbe9b58b037463f14a589d17c9901b11
Rob Winterburn <forestalwinters@gmail.com>
2025-03-12 11:46:16 +0000

    updating routing

--- movies/app/index.tsx
+++ movies/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -5,6 +6,9 @@ export default function Index() {
     <View className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-accent font-bold">Is it working?.</Text>
+      <Link
+      href="/onboarding">onboarding</Link>
+      <Link href="/movie/avengers">Avengers Movie</Link>
     </View>
   );
 }
